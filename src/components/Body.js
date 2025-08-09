18491afb9e8966df508f92f11590cbc1d57285bc
Balu <pbsn99@gmail.com>
2025-08-09 10:49:39 -0400

    New Changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -67,7 +67,7 @@ const Body = () => {
             const filteredList = listOfRestaurants.filter(
               (res) => res.info.avgRating >= 4.5
             );
-            setListOfRestaurants(filteredList);
+            setFilteredRestaurant(filteredList);
           }}
         >
           Top Rated Restaurants
